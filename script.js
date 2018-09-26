439308be28b34c0d66e78cf78ba88e0654216a1a
android <android@localhost.localdomain>
2018-09-26 18:48:47 +0200

    BetterMotherFuckingWebsite avec pop-up

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
-// var bouton_non = document.getElementById('non');
-// bouton_non.onClick = function(){
-// 	var backgr = document.getElementById('back-black')
-// 	backgr.remove('back-black');
-// }
-var non = document.getElementsByClassName('choix-non');
-console.log(non);
-for (var i = 0 ; i <= non.length ; i++) {
-	non.addEventListener('click',function(){
-		console.log('ok');
-	});
+setTimeout(function(){document.getElementById("back-black").style.display = "block"}, 3000);
+
+
+var non = document.getElementById('choix-non');
+	non.onclick = function (){
+		document.getElementById("back-black").style.display = "none";
+	}; 
+var oui = document.getElementById('choix-oui');
+	oui.onclick = function (){
+		document.getElementById("back-black").style.display = "none";
+	}; 
+
+var magenta = document.getElementById('back-magenta').getElementsByTagName('span');
+var bleu = document.getElementById('police-bleu').getElementsByTagName('span');
+var vert = document.getElementById('police-header-green').getElementsByTagName('span');
+var police = document.getElementById('changement-police').getElementsByTagName('span');
+
+magenta[0].onclick = function(){
+	document.querySelector('body').style.backgroundColor = "magenta";
+	document.getElementById('back-magenta').style.backgroundColor = "yellow";
+}
+
+bleu[0].onclick = function(){
+	var p = document.querySelector('body').getElementsByTagName('p');
+		for(var i = 0; i < p.length; i++){
+			p[i].style.color = "blue";
+		}
+	var h2 = document.querySelector('body').getElementsByTagName('h2');
+		for(var i = 0; i < h2.length; i++){
+			h2[i].style.color = "blue";
+		}
+	var h1 = document.querySelector('body').getElementsByTagName('h1');
+		for(var i = 0; i < h1.length; i++){
+			h1[i].style.color = "blue";
+		}
+}
+
+vert[0].onclick = function(){
+	var h1 = document.querySelector('body').getElementsByTagName('h1');
+		for(var i = 0; i < h1.length; i++){
+			h1[i].style.color = "green";
+			console.log(h1[i]);
+		}
+	var h2 = document.querySelector('body').getElementsByTagName('h2');
+		for(var i = 0; i < h2.length; i++){
+			h2[i].style.color = "green";
+		}
+	
+}
+
+police[0].onclick = function(){
+	var p = document.querySelector('body').getElementsByTagName('p');
+		for(var i = 0; i < p.length; i++){
+			p[i].style.fontFamily= "Sans-Serif";
+		}
+	var h2 = document.querySelector('body').getElementsByTagName('h2');
+		for(var i = 0; i < h2.length; i++){
+			h2[i].style.fontFamily = "Arial";
+		}
+	var h1 = document.querySelector('body').getElementsByTagName('h1');
+		for(var i = 0; i < h1.length; i++){
+			h1[i].style.fontFamily = "Arial";
+		}
+	
 }
